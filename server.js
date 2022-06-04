e1150d276dfaf4b4d04df542435b38be54d5b366
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-04 15:07:28 +0300

    comments

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// read the env. variables before app file
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-const { log } = require('console');
 
 const DB = process.env.DATABASE.replace(
 	'<PASSWORD>',
 	process.env.DATABASE_PASSWORD
 );
 
+// Connect DB
 mongoose.connect(DB).then(() => {
 	console.log('DB connection successful');
 });
 
 const port = process.env.PORT || 3000;
 
+// Start server
 const server = app.listen(port, () => {
 	console.log(`App running on port ${port}`);
 });
