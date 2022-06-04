1e2af28ba0f85fd9b9dd363ce924886cb93ec82c
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-04 15:07:23 +0300

    set up morgan

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const path = require('path');const express = require('express');
+const path = require('path');
+const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
 
+////////////////////////////////////////////////////////
+// Middlewares
+
 //serving static files
 // app.use(express.static(path.join(__dirname), 'public'));
 
+// HTTP requist logger
+//  lunux & macOS - "start:prod": "NODE_ENV=production node server.js"
+//  windows - "start:prod": "SET NODE_ENV=production & node server.js"
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
-//
+
 module.exports = app;
