a465da76a10ef6be06972c1d940dd93b9a882bab
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-05 12:52:11 +0300

    add create one

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,10 @@
+const catchAsync = require('../util/catchAsync');
+exports.createOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		const doc = await Model.create(req.body);
+
+		res.status(201).json({
+			status: 'success',
+			data: doc,
+		});
+	});
