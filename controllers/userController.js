5812927d755951c0b07678a2f6336cfca6377282
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-07 02:45:17 +0300

    update user data

---
+++ controllers/userController.js
@@ -0,0 +1,44 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../../Natours/utils/appError');
+const { status } = require('express/lib/response');
+const { findByIdAndUpdate } = require('../models/userModel');
+
+//filter the updated data
+const filterObj = (obj, allowedFields) => {
+	const newObj = {};
+	Object.keys(obj).forEach((el) => {
+		if (allowedFields.includes(el)) newObj[el] = obj[el];
+	});
+	return newObj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+	// create error if user post password data
+
+	if (req.body.password || req.body.passwordConfirm) {
+		return next(
+			new AppError(
+				'This route is not for password update. Please use /updateMyPassword',
+				400
+			)
+		);
+	}
+
+	//filter fields names that are not allowed to be updated
+	const allowedFields = ['firstName', 'lastName', 'email'];
+	const filteredBody = filterObj(req.body, allowedFields);
+
+	// update doc
+	const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			user: updatedUser,
+		},
+	});
+});
