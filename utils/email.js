55b2097ce3f84bc1a046ea043b4b06397aeb1ecb
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-06 23:03:43 +0300

    send email

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+	// create transporter
+	const transporter = nodemailer.createTransport({
+		host: process.env.EMAIL_HOST,
+		port: process.env.EMAIL_PORT,
+		auth: {
+			user: process.env.EMAIL_USERNAME,
+			password: process.env.EMAIL_PASSWORD,
+		},
+	});
+
+	// define email options
+	const mailOptions = {
+		from: 'Ivan Petrov <petur@gmail.com>',
+		to: options.email,
+		subject: options.subject,
+		text: options.messsage,
+	};
+
+	// send the email
+	//this is a promise
+	await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
