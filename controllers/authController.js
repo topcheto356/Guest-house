359240cabe8499298803c0900c62c7e03c1f67d2
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-06 18:08:08 +0300

    Not add unwanted data when sign up user

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,14 @@ const User = require('../models/userModel');
 const catchAsync = require('../util/catchAsync');
 
 exports.signUp = catchAsync(async (req, res, next) => {
-	const newUser = await User.create(req.body);
+	const newUser = await User.create({
+		//Not add unwanted data
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	});
 
 	res.status(201).json({
 		status: 'success',
