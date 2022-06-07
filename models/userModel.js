3da02143424127426b51f0f330488142b1ed9f69
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-07 20:38:43 +0300

    to use virtual properties

--- models/userModel.js
+++ models/userModel.js
@@ -3,58 +3,65 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-	firstName: {
-		type: String,
-		trim: true,
-		require: [true, 'User must have a name'],
-		maxlength: [30, 'Fist name must be less than 30 characters'],
-	},
-	lastName: {
-		type: String,
-		trim: true,
-		require: [true, 'User must have a name'],
-		maxlength: [30, 'Fist name must be less than 30 characters'],
-	},
-	email: {
-		type: String,
-		require: [true, 'User must have an email'],
-		unique: true,
-		validator: [validator.isEmail, 'Enter valid email'],
-	},
-	password: {
-		type: String,
-		require: [true, 'User must have a password'],
-		minlength: [8, 'A password must be minimum 8 characters'],
-		select: false,
-	},
-	passwordConfirm: {
-		type: String,
-		required: [true, 'Confirm your password'],
-		validate: {
-			//only work on CREATE and SAVE!!!
-			validator: function (el) {
-				return el === this.password;
-			},
-			message: 'Passwords are NOT the same',
+const userSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			trim: true,
+			require: [true, 'User must have a name'],
+			maxlength: [30, 'Fist name must be less than 30 characters'],
 		},
+		lastName: {
+			type: String,
+			trim: true,
+			require: [true, 'User must have a name'],
+			maxlength: [30, 'Fist name must be less than 30 characters'],
+		},
+		email: {
+			type: String,
+			require: [true, 'User must have an email'],
+			unique: true,
+			validator: [validator.isEmail, 'Enter valid email'],
+		},
+		password: {
+			type: String,
+			require: [true, 'User must have a password'],
+			minlength: [8, 'A password must be minimum 8 characters'],
+			select: false,
+		},
+		passwordConfirm: {
+			type: String,
+			required: [true, 'Confirm your password'],
+			validate: {
+				//only work on CREATE and SAVE!!!
+				validator: function (el) {
+					return el === this.password;
+				},
+				message: 'Passwords are NOT the same',
+			},
+		},
+		photo: String,
+		role: {
+			type: String,
+			enum: ['user', 'owner', 'admin'],
+			default: 'user',
+		},
+		active: {
+			//Used when deleting
+			type: Boolean,
+			default: true,
+			select: false,
+		},
+		passwordChangedAt: Date,
+		passwordResetToken: String,
+		passwordResetExpires: String,
 	},
-	photo: String,
-	role: {
-		type: String,
-		enum: ['user', 'owner', 'admin'],
-		default: 'user',
-	},
-	active: {
-		//Used when deleting
-		type: Boolean,
-		default: true,
-		select: false,
-	},
-	passwordChangedAt: Date,
-	passwordResetToken: String,
-	passwordResetExpires: String,
-});
+	{
+		//to use virtual properties
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
 
 //hashing the password
 userSchema.pre('save', async function (next) {
