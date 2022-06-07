a9ebba44e487c3125b2049576e8ed78b3a612018
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-07 14:08:30 +0300

    implement rate limiter

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const globalErrorHandler = require('./controllers/errorController');
 const houseRouter = require('./routes/houseRouter');
@@ -11,9 +12,6 @@ const app = express();
 ////////////////////////////////////////////////////////
 // Middlewares
 
-//serving static files
-// app.use(express.static(path.join(__dirname), 'public'));
-
 // HTTP requist logger
 //  lunux & macOS - "start:prod": "NODE_ENV=production node server.js"
 //  windows - "start:prod": "SET NODE_ENV=production & node server.js"
@@ -21,12 +19,23 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+//rate timiter
+const limiter = {
+	max: 100,
+	windowMs: 60 * 60 * 1000, //1h
+	message: 'Too many requests from this IP, please try again in an hour',
+};
+app.use('/api', limiter);
+
+//serving static files
+// app.use(express.static(path.join(__dirname), 'public'));
+
 //body parcer
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json());
 ////////////////////////////////////////////////////////
 //Routes
-app.use('/houses', houseRouter);
-app.use('/users', userRouter);
+app.use('/api/houses', houseRouter);
+app.use('/api/users', userRouter);
 
 app.use(globalErrorHandler);
 module.exports = app;
