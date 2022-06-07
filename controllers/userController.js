17fd31db870154b02e67ead2d992b64cd90bd0f6
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-07 13:47:00 +0300

    "delete" current user
    not really deleting , just making it inactive

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,6 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../../Natours/utils/appError');
-const { status } = require('express/lib/response');
-const { findByIdAndUpdate } = require('../models/userModel');
 
 //filter the updated data
 const filterObj = (obj, allowedFields) => {
@@ -40,5 +38,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 		data: {
 			user: updatedUser,
 		},
+	});
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+	await User.findByIdAndUpdate(req.user.id, { active: false });
+
+	res.status(204).json({
+		status: 'success',
+		data: null,
 	});
 });
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -18,4 +18,6 @@ router.patch(
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
 
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 module.exports = router;
