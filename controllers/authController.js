eb89358bf5fd4ba1ef72c068075d5a7e60ea8553
topcheto356 <92276015+topcheto356@users.noreply.github.com>
2022-06-07 13:59:40 +0300

    send token via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,6 +18,22 @@ const signToken = (id) => {
 const createAndSendToken = (user, statusCode, res) => {
 	const token = signToken(user._id);
 
+	const cookieOptions = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+		),
+		//cannot be access or change by the browzer
+		httpOnly: true,
+	};
+
+	//only if production add .secure
+	if (process.env.NODE_ENV === 'production') {
+		//only send in https
+		cookieOptions.secure = true;
+	}
+
+	res.cookie('jwt', token, cookieOptions);
+
 	res.status(statusCode).json({
 		status: 'success',
 		token,
